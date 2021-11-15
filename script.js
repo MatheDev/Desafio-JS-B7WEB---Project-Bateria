cf19ceb951fc17d3c2b430030458246b22abf0e9
MatheDev <matheusdeveloperr@gmail.com>
2021-11-15 12:17:38 -0300

    sequência input completa

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+// Events
+
 // adding an observer to page events
 document.body.addEventListener('keyup' ,(event)=> {
   playSound(event.code.toLowerCase());
 });
 
 
+// composer
+document.querySelector('.composer button').addEventListener('click', () => {
+  let song = document.querySelector('#input').value;
+
+  if(song !== ''){
+    let songArray = song.split('');
+    playComposition(songArray);
+  }
+  
+});
 
 
 // Functions
+
 
 // Play sound
 function playSound(sound){
@@ -25,4 +38,17 @@ function playSound(sound){
           keyElement.classList.remove('active');
         }, 300 )
     }
+}
+
+function playComposition(songArray){
+    let wait = 0;
+
+    for(let songItem of songArray){
+        setTimeout( () => {
+            playSound(`key${songItem}`);
+        }, wait);
+
+        wait += 250;
+    }
+
 }
